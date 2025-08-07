20f24d67adeff3eac42d1331fd0a49d2f27543f9
dhanasekaranvasan <dhanasekaran.vasan@gmail.com>
2025-08-07 14:42:54 +0530

    Added Rest Path

--- src/main/java/com/example/GithubJenkinsDockerApplication.java
+++ src/main/java/com/example/GithubJenkinsDockerApplication.java
@@ -2,12 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubJenkinsDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubJenkinsDockerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String getRoot() {
+		return "Welcome to Github Jenkins Docker AWS Project";
+	}
 }
